0be1f60000ff41e6b57e18b8d5516cb20c1298b9
javi-teaches <japsolo@gmail.com>
2022-10-13 09:20:42 -0500

    cambios sugeridos en code review

--- register.js
+++ register.js
@@ -1,5 +1,5 @@
 console.log("trabajo en register");
 console.log("trabajo en register");
-console.log("trabajo en register");
+console.info("Cambié esto");
 console.log("trabajo en register");
 console.log("trabajo en register");
